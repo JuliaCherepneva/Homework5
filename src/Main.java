3709aae8b47a3e571342d0ca1251685b24a7fc19
JuliaCherepnev <yuliya.cherepnev@mail.ru>
2024-04-28 10:39:38 +0400

    Исправленное домашнее задание

--- src/Main.java
+++ src/Main.java
@@ -3,20 +3,19 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Задание 1");
-        char clientOS = 0;
+        int clientOS = 0;
         if (clientOS == 0) {
             System.out.println("Установите версию приложения для iOS по ссылке");
         } else if (clientOS == 1) {
             System.out.println("Установите версию приложения для Android по ссылке");
         }
         System.out.println("\nЗадание 2");
-        char clientOs = 1;
         int clientDeviceYear = 2015;
-        if (clientOs == 0 && clientDeviceYear < 2015) {
+        if (clientOS == 0 && clientDeviceYear < 2015) {
             System.out.println("Установите облегченную версию приложения для iOS по ссылке");
-        } else if (clientOs == 0 && clientDeviceYear >= 2015) {
+        } else if (clientOS == 0 && clientDeviceYear >= 2015) {
             System.out.println("Установите версию приложения для iOS по ссылке");
-        } else if (clientOs == 1 && clientDeviceYear < 2015) {
+        } else if (clientOS == 1 && clientDeviceYear < 2015) {
             System.out.println("Установите облегченную версию приложения для Android по ссылке");
         } else {
             System.out.println("Установите версию приложения для Android по ссылке");
